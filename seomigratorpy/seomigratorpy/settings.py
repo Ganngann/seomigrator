274d6cf342d5b0f280fc6a48886eb7c058afb182
Morgan Schaefer <morgan.schaefer@djmdigital.be>
2023-12-04 02:23:30 +0100

    Statifiles

--- seomigratorpy/seomigratorpy/settings.py
+++ seomigratorpy/seomigratorpy/settings.py
@@ -30,8 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
-
+DEBUG = os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
@@ -93,11 +92,6 @@ DATABASES = {
         'PORT': os.getenv('SQL_PORT', '3306'),
     }
 }
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-if DEBUG:
-    STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Password validation
@@ -134,11 +128,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'migrator/static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+if DEBUG:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
